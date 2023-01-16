f158207ade0c508c5485fb1a75e230f59fdc9cba
Leonardo Nóbrega <77946734+LeonardoSCN@users.noreply.github.com>
2023-01-16 23:41:21 +0000

    Add files via upload
    Form js

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -11,3 +11,19 @@ document.querySelectorAll(".menu-link").forEach(e => e.addEventListener("click",
   hamburguer.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+/* Script to send email */
+
+function sendEmail(){
+  Email.send({
+    SecureToken: "c7a76c89-57ca-4bb9-9f33-983979115380",
+    To : "leo.coreyy@gmail.com",
+    From : document.getElementById("email").value,
+    Subject : "New Contact Form",
+    Body : "Name: " + document.getElementById("name").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> Message: " + document.getElementById("message").value
+}).then(
+  message => alert("Message Sent Succesfully :)")
+);
+}
